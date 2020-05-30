ce5a515f71f4a0640a1fa9254cd4260e2b9f2525
Wanlu Niu <niu.wanlu@rea-group.com>
2020-05-30 13:43:49 +0800

    Wanlu: add api to login user

--- workshop/services.py
+++ workshop/services.py
@@ -1,6 +1,10 @@
 import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.db.utils import IntegrityError
+
 from .models import User
 
 
@@ -8,6 +12,7 @@ def index(request):
     return HttpResponse("Hello, world. You're at the workshop index.")
 
 
+@login_required
 def list_users(request):
     users = User.objects.all()
     return HttpResponse(users, content_type="text/plain")
@@ -18,5 +23,23 @@ def register_user(request):
     data = json.loads(request.body.decode('utf-8'))
     username = data.get('username', '')
     password = data.get('password', '')
-    User.objects.create_user(username=username, password=password)
+    try:
+        User.objects.create_user(username=username, password=password)
+    except ValueError as e:
+        return HttpResponse(e, status=400)
+    except IntegrityError as e:
+        return HttpResponse(e, status=400)
     return HttpResponse()
+
+
+@csrf_exempt
+def login_user(request):
+    data = json.loads(request.body.decode('utf-8'))
+    username = data.get('username', '')
+    password = data.get('password', '')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse()
+    else:
+        return HttpResponse(status=401)
--- workshop/urls.py
+++ workshop/urls.py
@@ -4,6 +4,7 @@ from . import services
 
 urlpatterns = [
     path('', services.index, name='index'),
-    path('users/', services.list_users, name='users'),
-    path('users/register', services.register_user, name='users'),
+    path('users', services.list_users, name='users'),
+    path('users/register', services.register_user, name='register'),
+    path('users/login', services.login_user, name='login'),
 ]
