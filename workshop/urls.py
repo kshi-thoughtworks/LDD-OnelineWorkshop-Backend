ae35151bfe42e7f0411abea42ce3924aa9099700
Pei Luo <pei.luo@thoughtworks.com>
2020-06-23 19:26:17 +0800

    rename the url

--- workshop/urls.py
+++ workshop/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('elements/<int:element_id>/copy', services.copy_element_by_id, name='copy_element_by_id'),
     path('steps/<int:step_id>/elements', services.list_elements_by_step, name='list_elements_by_step'),
     path('cards', services.get_cards, name='get_cards'),
+    path('cards/tools', services.get_tools_cards, name='get_tools_cards'),
     path('cards/types', services.get_card_types, name='get_card_types'),
     path('cards/types/<str:card_tpye>', services.get_cards_by_type, name='get_cards_by_type'),
-    path('cards/types/tools', services.get_tools_cards, name='get_tools_cards'),
     url(r'^.*?$', TemplateView.as_view(template_name='index.html')),
 ]
