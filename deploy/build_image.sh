7249b1421eeadbf29797fc59217b68afac432c12
Pei Luo <pei.luo@thoughtworks.com>
2020-06-28 17:52:27 +0800

    add env params

--- deploy/build_image.sh
+++ deploy/build_image.sh
@@ -1,3 +1,6 @@
 cp deploy/prod.settings.py server/settings.py
+echo "user and pwd:"
+echo ${LDD_DB_USER}
+echo ${LDD_DB_PASSWORD}
 docker build --build-arg USER=${LDD_DB_USER} --build-arg PW=${LDD_DB_PASSWORD} -t ldd-app .
 docker image prune
